f3d41a87b81078222dc6a8f591246fd496d3657a
David Sun <15901110328@139.com>
2023-09-20 13:43:06 -0600

    Adding Title&Win&Lose Screens

--- Assets/Scripts/Goalpoint.cs
+++ Assets/Scripts/Goalpoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goalpoint : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class Goalpoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        over = endscreen.transform.Find("end");
+        over = endscreen.transform.Find("win");
     }
 
     // Update is called once per frame
@@ -31,14 +32,36 @@ public class Goalpoint : MonoBehaviour
             cubeB = true;
         }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "cubeA")
+        {
+            cubeA = false;
+        }
+        if (collision.gameObject.tag == "cubeB")
+        {
+            cubeB = false;
+        }
+    }
 
     private void Win()
     {
         if (cubeA && cubeB) 
         {
-            Time.timeScale = 0f;
+            //Time.timeScale = 0f;
             Debug.Log("You Win");
             over.gameObject.SetActive(true);
         }
+    }
+    public void Playagain()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Start Menu");
     }
 }
