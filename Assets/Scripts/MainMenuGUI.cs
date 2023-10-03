57c06d2f607258f2fa4d93597e113556f26b88a8
BackgroundWhimsy <81605720+BackgroundWhimsy@users.noreply.github.com>
2023-10-03 10:44:55 -0600

    How to Play Screen

--- Assets/Scripts/MainMenuGUI.cs
+++ Assets/Scripts/MainMenuGUI.cs
@@ -7,12 +7,18 @@ public class MainMenuGUI : MonoBehaviour
 {
     [SerializeField] Button startButton;
     [SerializeField] Button tutorialButton;
+    [SerializeField] Button backButton;
+    [SerializeField] GameObject mainSprites;
+    [SerializeField] GameObject controlsOne;
 
     // Start is called before the first frame update
     void Start()
     {
         startButton.gameObject.SetActive(true);
         tutorialButton.gameObject.SetActive(true);
+        mainSprites.SetActive(true);
+        controlsOne.SetActive(false);
+        backButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +31,17 @@ public class MainMenuGUI : MonoBehaviour
     {
         startButton.gameObject.SetActive(false);
         tutorialButton.gameObject.SetActive(false);
+        mainSprites.SetActive(false);
+        controlsOne.SetActive(true);
+        backButton.gameObject.SetActive(true);
+    }
+
+    public void HideTutorial()
+    {
+        startButton.gameObject.SetActive(true);
+        tutorialButton.gameObject.SetActive(true);
+        mainSprites.SetActive(true);
+        controlsOne.SetActive(false);
+        backButton.gameObject.SetActive(false);
     }
 }
